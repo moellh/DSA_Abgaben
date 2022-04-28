ab8800e827f8eee0272de1d90f6168ad6c59f487
Henrik Möllmann <henmoell@gmail.com>
2022-04-28 19:14:29 +0200

    Aufg. 4: Implementierung von a), b), c) aber noch ohne Kommentare (mit MainTest.java kann getestet werden)

--- Ex02_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex02/p4/Complexity.java
+++ Ex02_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex02/p4/Complexity.java
@@ -9,7 +9,7 @@ public class Complexity {
 	 * @param n positive integer n
 	 * @return some new number based on n
 	 */
-	public static int couldBeBetter1(int n) {
+	public static int couldBeBetter1(final int n) {
 		int result = 0;
 		for (int i = 0; i <= n; i++) {
 			result += 2;
@@ -23,7 +23,7 @@ public class Complexity {
 	 * @param n positive integer n
 	 * @return some new number based on n
 	 */
-	public static int couldBeBetter2(int n) {
+	public static int couldBeBetter2(final int n) {
 		int temp = 1;
 		int result = 1;
 		for (int i = 1; i <= n; i++) {
@@ -41,8 +41,7 @@ public class Complexity {
 	 * @param n positive integer
 	 * @return some new number based on n
 	 */
-	public static int couldBeBetter3(int n) {
-		
+	public static int couldBeBetter3(final int n) {
 		if (n < 0) throw new IllegalArgumentException("n has to be positive!");
 		else if (n <= 1) return 1;
 		else return couldBeBetter3(n-1)+ couldBeBetter3(n-2);
@@ -54,8 +53,8 @@ public class Complexity {
 	 * @param n positive integer
 	 * @return same number as couldBeBetter1, but faster
 	 */
-	public static int isDoneBetter1(int n) {
-
+	public static int isDoneBetter1(final int n) {
+		return 2*n + 2;
 	}
 	
 	
@@ -64,8 +63,14 @@ public class Complexity {
 	 * @param n positive integer
 	 * @return same number as couldBeBetter1, but faster
 	 */
-	public static int isDoneBetter2(int n) {
-
+	public static int isDoneBetter2(final int n) {
+		int temp = 1;
+		int result = 1;
+		for (int i = 1; i <= n; i++) {
+			result = temp*i;
+			temp = result;
+		}
+		return result;
 	}
 
 	
@@ -74,8 +79,21 @@ public class Complexity {
 	 * @param n positive integer
 	 * @return same number as couldBeBetter1, but faster
 	 */
-	public static int isDoneBetter3(int n) {
-
+	public static int isDoneBetter3(final int n) {
+		if (n < 0) {
+			throw new IllegalArgumentException("n has to be positive!");
+		}
+		
+		int lowerNumber = 1;
+		int higherNumber = 1;
+		
+		for(int i = 0; i<n-1; i++) {
+			final int tempNumber = higherNumber;
+			higherNumber += lowerNumber;
+			lowerNumber = tempNumber;
+		}
+		
+		return higherNumber;
 	}
 
 }
