3b27ac492e0189c4587c7718d0720ec4f2b4f1bc
Henrik Möllmann <henmoell@gmail.com>
2022-04-29 17:23:12 +0200

    UB02 - Aufg.4+5: finished implementation

--- Ex02_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex02/p5/SimpleList.java
+++ Ex02_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex02/p5/SimpleList.java
@@ -1,32 +1,82 @@
 package de.unistuttgart.dsass2022.ex02.p5;
 
-
+/**
+ * Implementation of a linked list that uses bubbleSort for ({@link #sort()}})
+ * 
+ * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+ * @param <T> Type of Elements
+ */
 public class SimpleList<T extends Comparable<T>> implements ISimpleList<T> {
 	
-	
+	private final ISimpleListNode<T> head;
+	private int size;
 
+	/**
+	 * Constructor for an empty SimpleList.
+	 */
+	public SimpleList() {
+		head = new SimpleListNode<T>();
+		size = 0;
+	}
 
 	@Override
 	public int getSize() {
-
+		return size;
 	}
 
 	@Override
-	public void prepend(T element) {
-
+	public void prepend(final T element) {
+		final ISimpleListNode<T> newSecondNode = head.getNext();
+		final ISimpleListNode<T> newFirstNode = new SimpleListNode<T>(element, newSecondNode);
+		head.setNext(newFirstNode);
+		size++;
 	}
 
 	@Override
-	public T getElement(int index) {
-
+	public T getElement(final int index) {
+		if(index >= getSize()) {
+			throw new IndexOutOfBoundsException();
+		}
+		ISimpleListNode<T> node = head.getNext();
+		for(int i = 0; i < index; i++) {
+			node = node.getNext();
+		}
+		return node.getElement();
 	}
-
 
 	@Override
 	public void sort() {
-
+		boolean noSwapsThisIteration = false;
+		while(!noSwapsThisIteration) {
+			noSwapsThisIteration = true;
+			ISimpleListNode<T> previousNode = head;
+			for(int i = 0; i<size-1; i++) {
+				final ISimpleListNode<T> currentNode = previousNode.getNext();
+				final ISimpleListNode<T> nextNode = currentNode.getNext();
+				if(currentNode.getElement().compareTo(nextNode.getElement()) > 0) {
+					swapConsecutiveNodes(previousNode);
+					noSwapsThisIteration = false;
+				}
+				
+				previousNode = previousNode.getNext();
+			}
+		}
 	}
-
 	
-
+	/**
+	 * Swaps the two nodes following {@link #previousNode}, if both of them are not null.
+	 * 
+	 * @param previousNode
+	 */
+	private void swapConsecutiveNodes(final ISimpleListNode<T> previousNode) {
+		final ISimpleListNode<T> currentNode = previousNode.getNext();
+		final ISimpleListNode<T> nextNode = currentNode.getNext();
+		
+		assert(currentNode != null && nextNode != null);
+		
+		previousNode.setNext(nextNode);
+		currentNode.setNext(nextNode.getNext());
+		nextNode.setNext(currentNode);
+	}
+	
 }
--- Ex02_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex02/p5/SimpleListNode.java
+++ Ex02_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex02/p5/SimpleListNode.java
@@ -1,26 +1,52 @@
 package de.unistuttgart.dsass2022.ex02.p5;
 
+/**
+ * Implementation for a Node of a simple Linked List. 
+ * 
+ * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+ * @param <T> Type of Elements
+ */
 public class SimpleListNode<T extends Comparable<T>> implements ISimpleListNode<T>{
+	private T element;
+	private ISimpleListNode<T> next;
 	
-
-	@Override
-	public T getElement() {
-
+	/**
+	 * Constructor for SimpleListNode
+	 * 
+	 * @param element
+	 * @param nextNode
+	 */
+	public SimpleListNode(final T element, final ISimpleListNode<T> nextNode) {
+		this.element = element;
+		this.next = nextNode;
+	}   
+	
+	/**
+	 * Constructor for not-initialized SimpleListNode
+	 */
+	public SimpleListNode() {
+		element = null;
+		next = null;
 	}
 
 	@Override
-	public void setElement(T element) {
+	public T getElement() {
+		return element;
+	}
 
+	@Override
+	public void setElement(final T element) {
+		this.element = element;
 	}
 
 	@Override
 	public ISimpleListNode<T> getNext() {
-
+		return next;
 	}
 
 	@Override
-	public void setNext(ISimpleListNode<T> node) {
-
+	public void setNext(final ISimpleListNode<T> node) {
+		this.next = node;
 	}
 
 }
