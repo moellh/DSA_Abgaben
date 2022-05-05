480ccd40721284bc02926faca11748bfafd31ea9
Henrik Möllmann <henmoell@gmail.com>
2022-05-06 00:24:27 +0200

    added Ex03 and changed project-names

---
+++ Ex03_MoellmannMuerwald_project/src/test/java/de/unistuttgart/dsass2022/ex03/p5/BinaryTreeNodeTest.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.dsass2022.ex03.p5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class BinaryTreeNodeTest {
+
+
+}
