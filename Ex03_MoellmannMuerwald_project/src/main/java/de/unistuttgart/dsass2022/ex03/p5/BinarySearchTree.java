4d550c0cbdcb15cf001b5d4739289d7b8abb1853
Henrik Möllmann <henmoell@gmail.com>
2022-05-06 14:37:43 +0200

    UB03 - full implementation of all excercises

--- Ex03_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex03/p5/BinarySearchTree.java
+++ Ex03_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex03/p5/BinarySearchTree.java
@@ -1,11 +1,16 @@
 package de.unistuttgart.dsass2022.ex03.p5;
 
+import java.util.Deque;
 import java.util.Iterator;
-//START SOLUTION
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
+/**
+ * 
+ * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+ *
+ * @param <T>
+ */
 public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchTreeIterable<T> {
 
 	private volatile IBinaryTreeNode<T> root;
@@ -15,13 +20,13 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 	}
 
 	@Override
-	public void insert(T t) {
+	public void insert(final T t) {
 		this.root = this.insert(this.root, t);
 	}
 
-	private IBinaryTreeNode<T> insert(IBinaryTreeNode<T> node, T t) {
+	private IBinaryTreeNode<T> insert(final IBinaryTreeNode<T> node, final T t) {
 		if (node == null) {
-			IBinaryTreeNode<T> newNode = new BinaryTreeNode<>();
+			final IBinaryTreeNode<T> newNode = new BinaryTreeNode<>();
 			newNode.setValue(t);
 			return newNode;
 		}
@@ -36,6 +41,222 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 	@Override
 	public IBinaryTreeNode<T> getRootNode() {
 		return this.root;
+	}
+
+	@Override
+	public boolean isFull() {
+		return nodeHasFullNodes(root);
+	}
+	
+	/**
+	 * Returns whether subtree of {@link #parentNode} is a 'full' tree
+	 * 
+	 * @param parentNode
+	 * @return true if substree of {@link #parentNode}.
+	 */
+	private boolean nodeHasFullNodes(final IBinaryTreeNode<T> parentNode) {
+		if( parentNode == null || (parentNode.getLeftChild() == null && parentNode.getRightChild() == null) ) {
+			return true;
+		}
+		
+		if( parentNode.getLeftChild() != null && parentNode.getRightChild() != null ) {
+			return nodeHasFullNodes(parentNode.getLeftChild()) && nodeHasFullNodes(parentNode.getRightChild());
+		}
+		
+		return false;
+	}
+
+	@Override
+	public Iterator<T> iterator(final TreeTraversalType traversalType) {
+		switch(traversalType) {
+		case PREORDER: return new PreorderIterator(this);
+		case INORDER: return new InorderIterator(this);
+		case POSTORDER: return new PostorderIterator(this);
+		default: return new LevelorderIterator(this);
+		}
+	}
+	
+	/**
+	 * Iterates through values of this BinarySearchTree in Preorder-traversal:
+	 * <p>
+	 * Regarding a parent-node:<br>
+	 * * 1. value of PARENT-node<br>
+	 * * 2. values of LEFT subtree in Preorder-traversal<br>
+	 * * 3. values of RIGHT subtree in Preorder-traversal
+	 * 
+	 * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+	 */
+	private class PreorderIterator implements Iterator<T>{
+		Deque<IBinaryTreeNode<T>> stack = new LinkedList<>();
+
+		/**
+		 * Creates an Iterator of {@link #tree} that iterates in Preorder-traversal.
+		 * @param tree
+		 */
+		public PreorderIterator(final IBinarySearchTree<T> tree) {
+			if(tree.getRootNode() != null){ 
+				stack.push(tree.getRootNode());
+			}
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return !stack.isEmpty();
+		}
+
+		@Override
+		public T next() {
+			final IBinaryTreeNode<T> node = stack.pop();
+			final T value = node.getValue();
+			if(node.getRightChild() != null){
+				stack.push(node.getRightChild());
+			}
+			if(node.getLeftChild() != null){
+				stack.push(node.getLeftChild());
+			}
+			return value;
+		}
+		
+	}
+	
+	/**
+	 * Iterates through values of this BinarySearchTree in Inorder-traversal:
+	 * <p>
+	 * Regarding a parent-node:<br>
+	 * * 1. values of LEFT subtree in Inorder-traversal<br>
+	 * * 2. value of PARENT-node<br>
+	 * * 3. values of RIGHT subtree in Inorder-traversal
+	 * 
+	 * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+	 */
+	private class InorderIterator implements Iterator<T>{
+		Deque<IBinaryTreeNode<T>> stack = new LinkedList<>();
+
+		/**
+		 * Creates an Iterator of {@link #tree} that iterates in Inorder-traversal.
+		 * @param tree
+		 */
+		public InorderIterator(final BinarySearchTree<T> binarySearchTree) {
+			pushSubtreeToStack(binarySearchTree.getRootNode());
+		} 
+	
+		@Override
+		public boolean hasNext() {
+			return !stack.isEmpty();
+		}
+
+		@Override
+		public T next() {
+			final IBinaryTreeNode<T> node = stack.pop();
+			return node.getValue();
+		}
+		
+		/**
+		 * Pushes nodes of currentNode's subtree to {@link #stack} in Inorder-traversal.
+		 * 
+		 * @param currentNode
+		 */
+		private void pushSubtreeToStack(final IBinaryTreeNode<T> currentNode) {
+			if(currentNode.getRightChild() != null) {
+				pushSubtreeToStack(currentNode.getRightChild());
+			}
+			stack.push(currentNode);
+			if(currentNode.getLeftChild() != null) {
+				pushSubtreeToStack(currentNode.getLeftChild());
+			}
+		}
+		
+	}
+	
+	/**
+	 * Iterates through values of this BinarySearchTree in Postorder-traversal:
+	 * <p>
+	 * Regarding a parent-node:<br>
+	 * * 1. values of LEFT subtree in Postorder-traversal<br>
+	 * * 2. values of RIGHT subtree in Postorder-traversal<br>
+	 * * 3. value of PARENT-node
+	 * 
+	 * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+	 */
+	private class PostorderIterator implements Iterator<T>{
+		Deque<IBinaryTreeNode<T>> stack = new LinkedList<>();
+
+		/**
+		 * Creates an Iterator of {@link #tree} that iterates in Postorder-traversal.
+		 * @param tree
+		 */
+		public PostorderIterator(final IBinarySearchTree<T> tree) {
+			pushSubtreeToStack(tree.getRootNode());
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return !stack.isEmpty();
+		}
+
+		@Override
+		public T next() {
+			final IBinaryTreeNode<T> node = stack.pop();
+			return node.getValue();
+		}
+		
+		/**
+		 * Pushes nodes of currentNode's subtree to {@link #stack} in Postorder-traversal.
+		 * 
+		 * @param currentNode
+		 */
+		private void pushSubtreeToStack(final IBinaryTreeNode<T> currentNode) {
+			stack.push(currentNode);
+			if(currentNode.getRightChild() != null) {
+				pushSubtreeToStack(currentNode.getRightChild());
+			}
+			if(currentNode.getLeftChild() != null) {
+				pushSubtreeToStack(currentNode.getLeftChild());
+			}
+		}
+		
+	}
+	
+	/**
+	 * Iterates through values of this BinarySearchTree in Levelorder-traversal:
+	 * <p>
+	 * Regarding the root-node:<br>
+	 * * 1. value of root-node<br>
+	 * * 2. values 2nd niveau nodes<br>
+	 * * 3. values 3rd niveau nodes<br>
+	 * * ...
+	 * 
+	 * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+	 */
+	private class LevelorderIterator implements Iterator<T>{
+		Queue<IBinaryTreeNode<T>> queue = new LinkedList<>();		
+		
+		/**
+		 * Creates an Iterator of {@link #tree} that iterates in Levelorder-traversal.
+		 * @param tree
+		 */
+		public LevelorderIterator(final IBinarySearchTree<T> tree){
+			queue.add(tree.getRootNode());
+		}
+
+		@Override
+		public boolean hasNext() {
+			return !queue.isEmpty();
+		}
+
+		@Override
+		public T next() {
+			final IBinaryTreeNode<T> node = queue.poll();
+			if(node.getLeftChild() != null) {
+				queue.add(node.getLeftChild());
+			}
+			if(node.getRightChild() != null) {
+				queue.add(node.getRightChild());
+			}
+			
+			return node.getValue();
+		}
+		
 	}
 
 }
--- Ex03_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex03/p5/BinaryTreeNode.java
+++ Ex03_MoellmannMuerwald_project/src/main/java/de/unistuttgart/dsass2022/ex03/p5/BinaryTreeNode.java
@@ -1,10 +1,55 @@
 package de.unistuttgart.dsass2022.ex03.p5;
 
+/**
+ * A basic Binary-Tree Node that hold a value of type {@link #T}, a left and a right node.
+ * 
+ * @author Alina Mürwald (2838377), Henrik Möllmann (3537334)
+ *
+ * @param <T>
+ */
 public class BinaryTreeNode<T extends Comparable<T>> implements IBinaryTreeNode<T> {
 	
+	private T value;
+	private IBinaryTreeNode<T> leftNode, rightNode;
 	
+	/**
+	 * Creates an empty Binary-Tree Node that does not hold a value neighter a left or right node.
+	 * Those properties need to be assigned otherwise this BinaryTreeNode equals a <strong>nullNode<strong>.  
+	 */
 	public BinaryTreeNode() {
+		value = null;
+		leftNode = null;
+		rightNode = null;
+	}
 
+	@Override
+	public void setValue(final T val) {   
+		value = val;
+	}
+
+	@Override
+	public T getValue() {
+		return value;
+	}
+
+	@Override
+	public void setLeftChild(final IBinaryTreeNode<T> left) {
+		leftNode = left;
+	}
+
+	@Override
+	public IBinaryTreeNode<T> getLeftChild() {
+		return leftNode;
+	}
+
+	@Override
+	public void setRightChild(final IBinaryTreeNode<T> right) {
+		rightNode = right;
+	}
+
+	@Override
+	public IBinaryTreeNode<T> getRightChild() {
+		return rightNode;
 	}
 
 
